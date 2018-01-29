10265c77e0a33e4980b01832612934b0052761e1
mushonnifar <mushonnif3@gmail.com>
2018-01-29 22:36:03 +0700

    add function getIdentity

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
+use App\AccessTokens;
 
-class Controller extends BaseController
-{
+class Controller extends BaseController {
+
     //
+    protected function getIdentity(Request $request) {
+        $headers = $request->headers->all();
+        $token = $headers['x-access-token'];
+        $access_token = AccessTokens::where(['token' => $token])->first();
+        return $access_token;
+    }
+
 }
