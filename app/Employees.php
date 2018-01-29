fc5ff4f3c8d4dec49eee3f1e58df8a054e4570fd
mushonnifar <mushonnif3@gmail.com>
2018-01-29 19:41:39 +0700

    create migrate table

--- app/Employees.php
+++ app/Employees.php
@@ -1,20 +1,21 @@
-<?php namespace App;
+<?php
+
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Employees extends Model
-{
+class Employees extends Model {
+
     protected $fillable = ['name', 'email'];
-    static public function rules($id=NULL)
-    {
+
+    static public function rules($id = NULL) {
         return [
             'name' => 'required',
-            'email' => 'required|email|unique:employees,email,'.$id,
+            'email' => 'required|email|unique:employees,email,' . $id,
         ];
     }
 
-    static public function search($request)
-    {
+    static public function search($request) {
 
         $page = $request->input('page');
         $limit = $request->input('limit');
@@ -22,8 +23,8 @@ class Employees extends Model
 
         $search = $request->input('search');
 
-        if(isset($search)){
-            $params=$search;
+        if (isset($search)) {
+            $params = $search;
         }
 
         $limit = isset($limit) ? $limit : 10;
@@ -33,41 +34,41 @@ class Employees extends Model
         $offset = ($page - 1) * $limit;
 
         $query = Employees::select(['id', 'name', 'email', 'created_at', 'updated_at'])
-            ->limit($limit)
-            ->offset($offset);
+                ->limit($limit)
+                ->offset($offset);
 
-        if(isset($params['id'])) {
+        if (isset($params['id'])) {
             $query->where(['id' => $params['id']]);
         }
 
-        if(isset($params['created_at'])) {
+        if (isset($params['created_at'])) {
             $query->where(['created_at' => $params['created_at']]);
         }
-        if(isset($params['updated_at'])) {
+        if (isset($params['updated_at'])) {
             $query->where(['updated_at' => $params['updated_at']]);
         }
-        if(isset($params['name'])) {
-            $query->where('name','like',$params['name']);
+        if (isset($params['name'])) {
+            $query->where('name', 'like', $params['name']);
         }
-        if(isset($params['email'])){
-            $query->where('email','like',$params['email']);
+        if (isset($params['email'])) {
+            $query->where('email', 'like', $params['email']);
         }
 
 
-        if(isset($order)){
+        if (isset($order)) {
             $query->orderBy($order);
         }
 
-        $data=$query->get();
+        $data = $query->get();
 
 
         return [
-            'status'=>1,
+            'status' => 1,
             'data' => $data,
-            'page' => (int)$page,
+            'page' => (int) $page,
             'size' => $limit,
-            'totalCount' => (int)$data->count()
+            'totalCount' => (int) $data->count()
         ];
     }
+
 }
-?>
---
+++ database/migrations/2018_01_29_123125_create_menus_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent');
+            $table->string('name');
+            $table->string('description');
+            $table->string('link');
+            $table->integer('created_by');
+            $table->integer('deleted_by');
+            $table->integer('updated_by');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
